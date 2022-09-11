05d866548ec09c32974c803765307d998432e9be
cmembrez <cedric.membrez@gmail.com>
2022-09-11 18:56:43 +0200

    Fix: added SaveGame and Data in GameManager script

--- Assets/Scripts/GameData/GameManager.cs
+++ Assets/Scripts/GameData/GameManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 /// <summary>
@@ -10,6 +12,8 @@ public class GameManager : MonoBehaviour
 {
     private bool _gameStarted = false;
     private bool _gameFinished = false;
+    private PlayerData _playerData;
+    private string _savePathDir;
 
     [SerializeField]
     private GameObject[] puzzlesPrefabs;
@@ -21,8 +25,35 @@ public class GameManager : MonoBehaviour
     {
         if (!_gameStarted)
         {
+            _playerData = new PlayerData();
+            _playerData.PlayerID = "FJLF10";
+            _playerData.NumberOfPuzzlesStarted = 1;
+
             GameObject puzzle0 = Instantiate(puzzlesPrefabs[0], transform, false);
             _gameStarted = true;
         }
+    }
+
+    public void SaveGame()
+    {
+        SaveToJson(_playerData);
+    }
+
+
+    /// <summary>
+    /// Save the PlayerData as a JSON
+    /// </summary>
+    /// <param name="playerData"></param>
+    private void SaveToJson(PlayerData playerData)
+    {
+        // Full Path
+        _savePathDir = Application.persistentDataPath;
+        string fullPath = Path.Combine(_savePathDir, "PlayerData.json");
+
+        // Serialize
+        string jsonString = JsonUtility.ToJson(_playerData);
+        File.WriteAllText(fullPath, jsonString);
+
+        Debug.Log("Player data is saved under: " + fullPath);
     }
 }
---
+++ Assets/Scripts/GameData/PlayerData.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Data of the player
+/// </summary>
+[Serializable]
+public class PlayerData
+{
+    public string PlayerID;
+    public int NumberOfPuzzlesSolved;
+    public int NumberOfPuzzlesStarted;
+}
