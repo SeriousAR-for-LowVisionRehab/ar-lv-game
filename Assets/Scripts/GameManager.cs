d83fa9ba790654b6b8ff08bb100f10dce35970d7
cmembrez <cedric.membrez@gmail.com>
2022-10-03 17:17:01 +0200

    Chore: renamed and separated scenes w.r.t. research questions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.WorldLocking.Core;
 using System;
 using System.Collections.Generic;
@@ -17,13 +18,15 @@ public class GameManager : MonoBehaviour
     private WorldLockingManager _worldLockingManager { get { return WorldLockingManager.GetInstance(); } }
     private bool _isGameStarted = false;  // true if the player has started a new game
     // private bool _gameFinished = false;  // true if the player solved the entire escape room
-    [SerializeField] private bool _isGamePrepared = false;  // true if the escape room has been setup
+    private bool _isGamePrepared = false;  // true if the escape room has been setup
     private PlayerData _thePlayerData;
     private string _savePathDir;
     private string _playerDatafileName = "PlayerData.json";
     [SerializeField] private List<GameObject> _availablePuzzlesPrefabs;  // what the player has to solve
     [SerializeField] private List<GameObject> _availableToolsPrefabs;  // what the player can use to solve the puzzles
+    [SerializeField] private List<GameObject> _anchors;
 
+    public static GameManager Instance;
     public WorldLockingManager WorldLockingManager { get { return _worldLockingManager; } }
     public bool IsGameStarted
     {
@@ -38,7 +41,8 @@ public class GameManager : MonoBehaviour
     public PlayerData ThePlayerData {get { return _thePlayerData; }}
     public List<GameObject> AvailablePuzzlesPrefabs { get { return _availablePuzzlesPrefabs; } }
     public List<GameObject> AvailableToolsPrefabs { get { return _availableToolsPrefabs; } }    
-    public static GameManager Instance;
+    public List<GameObject> Anchors { get { return _anchors; } }
+    
 
     /// <summary>
     /// Awake() is called at the object's creation. Ensure that GameManager is a Singleton.
@@ -62,6 +66,16 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        Vector3 midpointAnchor01 = getMidpoint(_anchors[0].transform.position, _anchors[1].transform.position);
+        Debug.Log("[GameManager:Update] midpoint between cube0 and cube1: " + midpointAnchor01);
+        Debug.Log("[GameManager:Update] relative position puzzle 0 to cube 0: " + getRelativePosition(_anchors[0].transform, _availablePuzzlesPrefabs[0].transform.position));
+        Debug.Log("[GameManager:Update] relative position puzzle 0 to cube 1: " + getRelativePosition(_anchors[1].transform, _availablePuzzlesPrefabs[0].transform.position));
+        Debug.Log("[GameManager:Update] relative position puzzle 1 to cube 0: " + getRelativePosition(_anchors[0].transform, _availablePuzzlesPrefabs[1].transform.position));
+        Debug.Log("[GameManager:Update] relative position puzzle 1 to cube 1: " + getRelativePosition(_anchors[1].transform, _availablePuzzlesPrefabs[1].transform.position));
     }
 
 
@@ -104,4 +118,45 @@ public class GameManager : MonoBehaviour
         string json = File.ReadAllText(fullPath);
         _thePlayerData = JsonUtility.FromJson<PlayerData>(json);
     }
+
+    /// <summary>
+    /// Set the objectPosition to a new position referencePosition, instantly.
+    /// </summary>
+    /// <param name="referencePosition"></param>
+    /// <param name="objectPosition"></param>
+    public void resetPuzzle0ToMidpointAnchorAB()
+    {
+        Vector3 midpoint = getMidpoint(_anchors[0].transform.position, _anchors[1].transform.position);
+        _availablePuzzlesPrefabs[0].transform.position = midpoint;
+    }
+
+    /// <summary>
+    /// Return the position of the objectPosition relative to the referencePosition.
+    /// </summary>
+    /// <param name="referencePosition"></param>
+    /// <param name="objectPosition"></param>
+    /// <returns></returns>
+    private static Vector3 getRelativePosition(Transform referencePosition, Vector3 objectPosition)
+    {
+        Vector3 distanceDifference = objectPosition - referencePosition.position;
+
+        float relativePositionX = Vector3.Dot(distanceDifference, referencePosition.right.normalized);
+        float relativePositionY = Vector3.Dot(distanceDifference, referencePosition.up.normalized);
+        float relativePositionZ = Vector3.Dot(distanceDifference, referencePosition.forward.normalized);
+
+        return new Vector3(relativePositionX, relativePositionY, relativePositionZ);
+    }
+
+
+    /// <summary>
+    /// Return the midpoint position between positionOne and positionTwo
+    /// </summary>
+    /// <param name="positionOne"></param>
+    /// <param name="positionTwo"></param>
+    /// <returns></returns>
+    private static Vector3 getMidpoint(Vector3 positionOne, Vector3 positionTwo)
+    {
+        return Vector3.Lerp(positionOne, positionTwo, 0.5f);
+    }
+
 }
