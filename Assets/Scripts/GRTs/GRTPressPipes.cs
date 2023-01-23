4192dd8ee539e0fc465dce358e257c09c2f17573
cmembrez <cedric.membrez@gmail.com>
2023-01-23 21:14:44 +0100

    Feat: created Pipes' press script, extended Pipes' prefab

---
+++ Assets/Scripts/GRTs/GRTPressPipes.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GRTPressPipes : GRTPress
+{
+    protected override void FreezeGRTBox()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnUpdateSolving()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void UnfreezeGRTBox()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
